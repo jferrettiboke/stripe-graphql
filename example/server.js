730b63782047a8acb72ff3a9c017700ae916d8ed
Jesús Ferretti <jferrettiboke@gmail.com>
2018-10-03 00:28:42 +0200

    Add example project

--- example/server.js
+++ example/server.js
@@ -3,15 +3,15 @@ const { GraphQLServer } = require("graphql-yoga");
 const { Stripe } = require("stripe");
 const { importSchema } = require("graphql-import");
 
-// ????????
+// ???
 const { schema } = require("../src");
 
-const typeDefs = importSchema("./src/example/schema.graphql");
+const typeDefs = importSchema("./example/schema.graphql");
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
-    usCountrySpec: (root, args, context, info) => {
-      return delegateToSchema({
+    usCountrySpec: async (root, args, context, info) => {
+      return await delegateToSchema({
         schema,
         operation: "query",
         fieldName: "countrySpecs",
