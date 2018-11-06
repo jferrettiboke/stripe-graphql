b380232854f9b3983a74e3eb79169d9ddc85e9ef
Jesús Ferretti <jferrettiboke@gmail.com>
2018-11-06 13:49:59 +0100

    Add examples (WIP)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+const path = require("path");
 const { makeExecutableSchema } = require("graphql-tools");
 const { importSchema } = require("graphql-import");
+const { Binding } = require("graphql-binding");
 
-const typeDefs = importSchema("./src/schema.graphql");
-const resolvers = require("./resolvers");
+const typeDefs = importSchema(path.resolve(__dirname, "schema.graphql"));
+const { resolvers } = require("./resolvers");
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 });
 
-module.exports = { schema };
+class StripeServiceBinding extends Binding {
+  constructor() {
+    super({ schema });
+  }
+}
+
+module.exports = { schema, StripeServiceBinding };
