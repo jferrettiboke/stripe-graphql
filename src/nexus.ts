14269fef9a99c11e1097718294c30b8c48b12ee6
Jesús Ferretti <jferrettiboke@gmail.com>
2019-03-28 15:12:39 +0100

    Add stripeObjectType

---
+++ src/nexus.ts
@@ -0,0 +1,81 @@
+import { objectType } from "nexus";
+
+interface TypeFields {
+  [key: string]: string | any;
+}
+
+const cardFieldTypeMapping: TypeFields = {
+  id: "ID",
+  exp_month: "Int",
+  exp_year: "Int",
+  brand: "String",
+  last4: "String"
+};
+
+interface Types {
+  [key: string]: TypeFields;
+}
+
+const types: Types = {
+  Card: cardFieldTypeMapping
+  // More types...
+};
+
+interface Field {
+  name: string;
+  alias: string;
+}
+
+interface StripeObjectType {
+  name: string;
+  fields: Array<string | Field>;
+  definition(t: any): any;
+}
+
+export function stripeObjectType(cfg: StripeObjectType) {
+  const { name, definition } = cfg;
+  let { fields } = cfg;
+
+  if (fields && fields.includes("*")) {
+    fields = Object.keys(types[name]).map(key => key);
+  }
+
+  return objectType({
+    name,
+    definition(t) {
+      fields.forEach(field => {
+        if (typeof field === "string") {
+          t.field(field, { type: types[name][field] });
+        } else if (field && typeof field === "object") {
+          t.field(field.alias, {
+            type: types[name][field.name],
+            resolve: root => root[field.name]
+          });
+        }
+      });
+      if (typeof definition === "function") {
+        definition(t);
+      }
+    }
+  });
+}
+
+// export const Card = stripeObjectType({
+//   name: "Card",
+//   fields: [
+//     "id",
+//     "brand",
+//     "last4",
+//     { name: "exp_month", alias: "expMonth" },
+//     { name: "exp_year", alias: "expYear" }
+//   ],
+//   definition(t) {
+//     t.field("filteredCharges", {
+//       type: "Invoice",
+//       list: true,
+//       resolve() {
+//         return [];
+//       }
+//     });
+//   }
+// });
