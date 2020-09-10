7fd7e1f01c2bb49cb7f45a6bc9df600cbfb160dd
ManUtopiK <emmanuel.salomon@gmail.com>
2020-09-10 03:20:28 +0200

    added auth guardian

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as glob from "glob";
 import { makeSchema } from "@nexus/schema";
+import { jwtAuthPlugin } from "./auth";
 
 let types = {};
 const files = glob.sync(path.resolve(__dirname, "./graphql/**/*.ts"));
@@ -9,12 +10,27 @@ files.forEach((file) => {
   types = { ...types, ...module };
 });
 
+const protectedPaths = [
+    'Query.balance',
+    'Query.balanceTransaction',
+    'Query.balanceTransactions',
+    'Query.charge',
+    'Query.charges',
+    'Query.customer',
+    'Query.customers',
+    'Query.plan',
+    'Query.plans'
+]
+
 const schema = makeSchema({
   types,
   outputs: {
     schema: path.join(__dirname, "./generated/schema.graphql"),
     typegen: path.join(__dirname, "./generated/types.d.ts"),
   },
+  plugins: [
+    jwtAuthPlugin(protectedPaths)
+  ],
   typegenAutoConfig: {
     debug: false,
     contextType: "t.Context",
